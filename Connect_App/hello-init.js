7d9aa73dbee67489f06d91675dec8e1a2cf24ebd
Rajeev <rajeev.msn@gmail.com>
2022-04-07 15:42:06 +0200

    Delete Connect_App directory

--- Connect_App/hello-init.js
+++
@@ -1,6 +0,0 @@
-hello.init({
-    connect: 'pub_xxxx' //Enter your client id
-}, {
-    oauth_proxy: 'http://localhost:3500/oauthproxy', //this is your local server
-    redirect_uri: 'redirect.html'
-});
