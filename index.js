ba1300496fe35f36f5f6c92f2763fafe2148c5e3
rajeevmsn <rajeev.msn@gmail.com>
2022-05-04 21:18:39 +0200

    Added two buttons, disconnect working, gettting token.

--- index.js
+++ index.js
@@ -39,13 +39,87 @@ const online = (session) => {
 };
 
 const tokenValue = (session) => session.access_token;
+const connectURL = 'https://connect-project.io';
 
-const sendData = () => {
-  alert('send data');
+const userId = (accessToken) => {
+
+  /*
+  curl --request GET \
+  --url $CONNECT_URL/oauth/user \
+  --header 'Authorization: Bearer '$access_token
+
+  Response:
+  { "id": "xxxx" }
+  */
+  const testGet = new XMLHttpRequest();
+  testGet.open('GET', `${connectURL}/oauth/user`);
+  testGet.setRequestHeader('Authorization', `Bearer ${accessToken}`);
+  testGet.onreadystatechange = function () {
+    if (testGet.readyState === 4) {
+      console.log(testGet.status);
+      console.log(testGet.responseText);
+    }
+  };
+  testGet.send();
 };
 
+/*
+const gameScore = (sessionToken, accessToken) => {
+
+  const getScore = new XMLHttpRequest();
+  getScore.open('GET', `${connectURL}/parse/classes/GameScore`);
+
+  getScore.setRequestHeader('x-parse-application-id', 'connect');
+  getScore.setRequestHeader('x-parse-session-token', `${sessionToken}`);
+  getScore.setRequestHeader('Authorization', `Bearer ${accessToken}`);
+
+  getScore.onreadystatechange = function () {
+    if (getScore.readyState === 4) {
+      console.log(getScore.status);
+      console.log(getScore.responseText);
+    }
+  };
+  getScore.send();
+};
+*/
 const getData = () => {
-  alert('get data');
+
+  // alert(getData);
+  const sessionInfo = hello('connect').getAuthResponse();
+  const accessToken = sessionInfo.access_token;
+  //const sessionToken = sessionInfo.session_state;
+  userId(accessToken);
+  //gameScore(sessionToken, accessToken);
+};
+
+const sendData = () => {
+  const timeStamp = new Date();
+  alert(timeStamp);
+
+  /*
+  const sessionInfo = hello('connect').getAuthResponse();
+  const accessToken = sessionInfo.access_token;
+  const objectId = userId(accessToken);
+  const sendScore = new XMLHttpRequest();
+  sendScore.open('GET', `${connectURL}/parse/classes/GameScore${objectId}`);
+
+  sendScore.setRequestHeader('content-type', 'application/json');
+  sendScore.setRequestHeader('x-parse-application-id', 'connect');
+  sendScore.setRequestHeader('Authorization', `Bearer ${accessToken}`);
+
+  sendScore.onreadystatechange = function () {
+    if (sendScore.readyState === 4) {
+      console.log(sendScore.status);
+      console.log(sendScore.responseText);
+    }
+  };
+  var data = `{
+    "score":1338,
+    "playerName":"sample",
+    "cheatMode":false,
+  }`;
+  sendScore.send(data);
+  */
 };
 
 const initHello = () => {
